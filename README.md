874b625841cf1f471dcc8e20e5d03cb5c2a911bf
Dominic <pib.dominic.holz@htw-saarland.de>
2013-03-06 13:53:59 +0100

    changed lanscanner

--- README.md
+++ README.md
@@ -2,3 +2,13 @@ startup
 =======
 
 dbvs startup program
+
+zu den Klassen:
+Ipconfig: ermittelt zu allen netzwerkgeräten die Ip adresse
+
+LanScanner:
+Scannt das lan anhand der aktuellen IP Adresse eines Adapters
+
+PortScanner (MainApp, PortScanner, Observernotifier)
+Scannt einen angegebenen Host und eine Portrange, ob diese erreichbar (offen) sind
+
--- src/LanScanner.java
+++ src/LanScanner.java
@@ -7,37 +7,52 @@ import java.util.Enumeration;
 
 public class LanScanner {
 	private static final int TIMEOUT = 15000;
-	private Object[] addresses = new Object[255];
+	private static Object[] addressesOld = new Object[255];
+	private static Object[] addressesNew = new Object[255];
 	private static int i=0;
 	
 
-	public static void main(String[] args) {
-		try {
-			;
-			Enumeration<NetworkInterface> enumInterfaces = NetworkInterface
-					.getNetworkInterfaces();
-			while (enumInterfaces.hasMoreElements()) {
-				NetworkInterface currentInterface = enumInterfaces
-						.nextElement();
-				if (currentInterface.isLoopback() == false
-						&& currentInterface.isUp()) {
-					Enumeration<InetAddress> enumAdresses = currentInterface
-							.getInetAddresses();
-					while (enumAdresses.hasMoreElements()) {
-						InetAddress currentAddress = enumAdresses.nextElement();
-						new LanScanner(currentAddress);
-						i++;
-					}
-				}
+//	public static void main(String[] args) {
+//		try {
+//			;
+//			Enumeration<NetworkInterface> enumInterfaces = NetworkInterface
+//					.getNetworkInterfaces();
+//			while (enumInterfaces.hasMoreElements()) {
+//				NetworkInterface currentInterface = enumInterfaces
+//						.nextElement();
+//				if (currentInterface.isLoopback() == false
+//						&& currentInterface.isUp()) {
+//					Enumeration<InetAddress> enumAdresses = currentInterface
+//							.getInetAddresses();
+//					while (enumAdresses.hasMoreElements()) {
+//						InetAddress currentAddress = enumAdresses.nextElement();
+//						new LanScanner(currentAddress);
+//						i++;
+//					}
+//				}
+//			}
+//			 Wenn man davon ausgeht, dass nur eine Netzwerkkarte aktiv ist,
+//			 dann nehmen wir die Kurzfassung:
+//			System.out.print(InetAddress.getLocalHost());
+//			
+//			new LanScanner(InetAddress.getLocalHost());
+//			
+//		} catch (SocketException e) {
+//			e.printStackTrace();
+//		}
+//	}
+
+		public static void main(String[] args) {
+			try {
+				new LanScanner(InetAddress.getLocalHost());
+				
+//				sortArray(addressesOld);
+			} catch (UnknownHostException e) {
+				e.printStackTrace();
 			}
-			// Wenn man davon ausgeht, dass nur eine Netzwerkkarte aktiv ist,
-			// dann nehmen wir die Kurzfassung:
-			// new LanScanner(InetAddress.getLocalHost());
-		} catch (SocketException e) {
-			e.printStackTrace();
-		}
 	}
 
+		
 	private LanScanner(InetAddress ia) {
 		String[] sAddress = ia.getHostAddress().split("[.]");
 		String lanAddress = sAddress[0] + "." + sAddress[1] + "." + sAddress[2]
@@ -45,8 +60,22 @@ public class LanScanner {
 		for (short s = 1; s < 255; s++) {
 			new AddressScanner(lanAddress + s);
 		}
-	}
+		
+	}	
+	private static void sortArray(Object[] array){
+			
+			java.util.Arrays.sort( array );
+			
+			  for (int n = 0; n < array.length; n++) {
+				  addressesNew[n] = array[n];
+				  System.out.println(addressesNew[n]);
+			  }
+		}
+	
 
+	
+	
+	
 	private class AddressScanner implements Runnable {
 		private String sInetAddress = null;
 
@@ -64,11 +93,13 @@ public class LanScanner {
 						|| !ia.getHostName()
 								.equalsIgnoreCase(this.sInetAddress)) {
 					
-					addresses[i] = this.sInetAddress;
+					addressesOld[i] = this.sInetAddress;
+					
 					
 //					System.out.println("Reached " + this.sInetAddress + "("
 //							+ ia.getCanonicalHostName() + ")");
-				System.out.println(addresses[i]);
+
+				System.out.println(addressesOld[i]);
 				}
 				
 			} catch (UnknownHostException e) {
@@ -77,5 +108,7 @@ public class LanScanner {
 				e.printStackTrace();
 			}
 		}
+		
+
 	}
 }
