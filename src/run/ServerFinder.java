0f0437f73b3e68577e00e1e1a5da68b7cde9c111
Dominic <pib.dominic.holz@htw-saarland.de>
2013-03-19 14:04:32 +0100

    organized imports

--- src/run/ServerFinder.java
+++ src/run/ServerFinder.java
@@ -7,11 +7,8 @@ import java.io.IOException;
 import java.net.Inet4Address;
 import java.net.InetAddress;
 import java.net.NetworkInterface;
-import java.net.SocketException;
 import java.net.UnknownHostException;
 import java.util.Enumeration;
-
-import sun.security.action.GetLongAction;
 
 public class ServerFinder {
 	
